52bf5038321f5d8f88966430a490fbe13e6d460d
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2025-06-06 16:47:01 -0300

    Fix typo in LogDebugWithCorrelationCheck

--- src/Playground.Application/Infrastructure/Extensions/LogDebugWithCorrelationCheck.cs
+++ src/Playground.Application/Infrastructure/Extensions/LogDebugWithCorrelationCheck.cs
@@ -7,10 +7,10 @@ namespace Playground.Application.Infrastructure.Extensions
     {
         public static void LogTroubleshooting(this ILogger logger, string message)
         {
-            Guid correlationIdToDebud = new("550e8400-1234-5678-9999-999900000001"); //TODO: Take from FeatureManager
+            Guid correlationIdToDebug = new("550e8400-1234-5678-9999-999900000001"); //TODO: Take from FeatureManager
             var correlationId = CorrelationContext.GetCorrelationId();
 
-            if (correlationId == correlationIdToDebud)
+            if (correlationId == correlationIdToDebug)
             {
                 logger.LogInformation(message);
             }
