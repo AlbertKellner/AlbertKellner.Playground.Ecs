6c7a018632529d497da140eb148032975d8c6b3d
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2025-06-09 23:54:52 -0300

    Add coverage step to build workflow

---
+++ src/Playground.Tests/Api/Controller/AuthController/GenerateTokenAuthControllerTest.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Playground.Controllers;
+
+namespace Playground.Tests.Controllers
+{
+    public class GenerateTokenAuthControllerTest
+    {
+        private readonly Mock<ILogger<AuthController>> _mockLogger;
+        private readonly AuthController _controller;
+
+        public GenerateTokenAuthControllerTest()
+        {
+            _mockLogger = new Mock<ILogger<AuthController>>();
+            _controller = new AuthController(_mockLogger.Object);
+        }
+
+        [Fact]
+        public void GenerateToken_DeveRetornarOkComToken()
+        {
+            var user = new AuthController.AuthUser("1", "tester", "group");
+
+            var actionResult = _controller.GenerateToken(user);
+
+            var response = Assert.IsType<OkObjectResult>(actionResult);
+            dynamic value = response.Value!;
+            Assert.NotNull(value.Token);
+            Assert.NotEqual(string.Empty, value.Token.ToString());
+        }
+    }
+}
