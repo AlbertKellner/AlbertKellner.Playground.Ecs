ab9e9e175a423145f39c18ecea194c220e55e0ed
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2025-06-20 10:23:04 -0300

    Improve handler performance using memory cache

--- src/Playground.Application/Features/ToDoItems/Query/GetById/UseCase/GetByIdToDoItemUseCaseHandler.cs
+++ src/Playground.Application/Features/ToDoItems/Query/GetById/UseCase/GetByIdToDoItemUseCaseHandler.cs
@@ -1,24 +1,34 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Caching.Memory;
 using Playground.Application.Features.ToDoItems.Query.GetById.Models;
 
 namespace Playground.Application.Features.ToDoItems.Query.GetById.UseCase
 {
     public class GetByIdToDoItemUseCaseHandler : IRequestHandler<GetByIdToDoItemQuery, GetByIdToDoItemOutput>
     {
+        private static readonly MemoryCache MemoryCache = new(new MemoryCacheOptions());
+
+        private static readonly GetByIdToDoItemOutput CachedItem = new()
+        {
+            Id = 99,
+            Task = "GetById - ToDoItem - UseCaseHandler",
+            IsCompleted = true
+        };
+
+        private const string CacheKey = "GetByIdToDoItem";
+
         public Task<GetByIdToDoItemOutput> Handle(GetByIdToDoItemQuery input, CancellationToken cancellationToken)
         {
-            var items = new List<GetByIdToDoItemOutput>
-            {
-                new GetByIdToDoItemOutput
-                {
-                    Id = 99,
-                    Task = "GetById - ToDoItem - UseCaseHandler",
-                    IsCompleted = true
-                }
-            };
+            if (input.Id != CachedItem.Id)
+                return Task.FromResult(new GetByIdToDoItemOutput());
 
-            var result = items.SingleOrDefault(item => item.Id == input.Id) ?? new GetByIdToDoItemOutput();
-            return Task.FromResult(result);
+            if (!MemoryCache.TryGetValue(CacheKey, out GetByIdToDoItemOutput? item))
+            {
+                item = CachedItem;
+                MemoryCache.Set(CacheKey, item, TimeSpan.FromMinutes(1));
+            }
+
+            return Task.FromResult(item!);
         }
     }
 }
